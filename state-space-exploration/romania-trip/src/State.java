c1dd8f69e27e8b8d8ea47452f8bcc3387d69d5f6
PierluigiCovone98 <covone.1895077@studenti.uniroma1.it>
2026-03-05 23:44:17 +0100

    Add the world representation

---
+++ state-space-exploration/romania-trip/src/State.java
@@ -0,0 +1,25 @@
+/**
+ * A state is label that identifies a city in Romania.
+ */
+public enum State {
+    ARAD,
+    BUCHAREST,
+    CRAIOVA,
+    DOBRETA,
+    EFORIE,
+    FAGARAS,
+    GIURGIU,
+    HIRSOVA,
+    IASI,
+    LUGOJ,
+    MEHADIA,
+    NEAMT,
+    ORADEA,
+    PITESTI,
+    RIMNICU_VILCEA,
+    SIBIU,
+    TIMISOARA,
+    URZICENI,
+    VASLUI,
+    ZERIND
+}
