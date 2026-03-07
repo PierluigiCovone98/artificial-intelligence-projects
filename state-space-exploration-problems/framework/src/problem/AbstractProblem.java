e7870935c672f07e860ad3dd9686df3788ba52e7
PierluigiCovone98 <covone.1895077@studenti.uniroma1.it>
2026-03-07 13:28:22 +0100

    Refactor the project: start building a framework for space-state-exploration problems

---
+++ state-space-exploration-problems/framework/src/problem/AbstractProblem.java
@@ -0,0 +1,52 @@
+package problem;
+
+import java.util.List;
+
+/**
+ * Formulation of a generic "Space of State Exploration" problem.
+ */
+public abstract class AbstractProblem<S, A> {
+
+    // Common fields
+    private final S initialState;
+    private final S objective;
+
+    /**
+     * Constructor.
+     * Initialize fields.
+     */
+    protected AbstractProblem(S initialState, S objective) {
+        this.initialState = initialState;
+        this.objective = objective;
+    }
+
+    /**
+     * Return all possible actions (of type A) from a given state (of type S).
+     */
+    public abstract List<A> getActions(S state);
+
+    /**
+     * Return the result of applying an action (of type A) to a state (of type S).
+     */
+    public abstract S getResult(S state, A action);
+
+    /**
+     * Get the step cost for a given state (of type S) and the performed action (of type A).
+     */
+    public abstract Number getStepCost(S state, A action);
+
+    /**
+     * Get initial state (of type S).
+     */
+    public S getInitialState() {
+        return initialState;
+    }
+
+    /**
+     * Determine if the given state (of type S) satisfies the objective.
+     */
+    public boolean goalTest(S state) {
+        return state.equals(this.objective);
+    }
+
+}
