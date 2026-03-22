948255eef5a76a4eb76e674a426c7e418d1a1951
PierluigiCovone98 <covone.1895077@studenti.uniroma1.it>
2026-03-22 18:20:03 +0100

    Add the Position class to the codebase

---
+++ src/it/uniroma1/ai/problems/proteinfolding/Position.java
@@ -0,0 +1,7 @@
+package it.uniroma1.ai.problems.proteinfolding;
+
+/**
+ * Pair of values representing coordinates of the 2D grid for the Protein Folding problem.
+ * The methods "equals()" and "hashCode()" are overridden by default.
+ */
+public record Position(int x, int y) { }
